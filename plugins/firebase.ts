1d3b237bf9eb7ce15884f9782e6f269910935d6c
Emil <rrotatew@gmail.com>
2025-12-04 12:12:09 +0700

    dockerfile

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -5,6 +5,7 @@ import {ref as dbRef} from "@firebase/database";
 export default defineNuxtPlugin((nuxtApp) => {
 
     const config : Record<string, string> = useRuntimeConfig()
+    console.log(config)
 
     const configFirebase: {databaseURL: string} = {databaseURL: config.public.databaseURL}
     const firebase = initializeApp(configFirebase);
