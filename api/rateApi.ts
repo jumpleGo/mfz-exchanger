a44ec6d0cfddc19150aa5f080dcccaf5849027d3
Emil <rrotatew@gmail.com>
2025-12-22 19:15:41 +0700

    crypto api

--- api/rateApi.ts
+++ api/rateApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const getPriceByTickers = async () => {
-  return await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_34DPnNwfsofhIluKiBRne1uEJAp0K3OzyEtcK0Om&currencies=RUB')
+  return await axios.get(`https://api.currencyapi.com/v3/latest?apikey=${process.env.NUXT_CRYPTO_CURRENCY_TOKEN}&currencies=RUB`)
 }
